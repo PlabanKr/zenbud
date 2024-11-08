cb46c2098ffc551a8628e37f0d46cad4e241c9c6
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-11-09 01:32:06 +0530

    Add string formatting for the list functions

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -187,28 +187,58 @@ pub fn add_new_routine(commands: &[&str], file_data: &mut data::DataFile) {
 
 fn list_all_tasks(file_data: &mut data::DataFile) {
     if file_data.tasks.is_empty() {
-        println!("no task found");
+        println!("No tasks found.");
+        return;
     }
+
+    println!("{:<50} | {:<10} | {:<15}", "Name", "Completed", "ETA");
+    println!("{}", "-".repeat(90));
+
     for task in &file_data.tasks {
-        println!("Name:{}\t|\tCompleted:{}\t|\tETA:{}", task.name, task.completed, task.eta)
+        println!(
+            "{:<50} | {:<10} | {:<15}",
+            task.name,
+            task.completed,
+            task.eta
+        );
     }
 }
 
 fn list_all_hobbies(file_data: &mut data::DataFile) {
     if file_data.hobbies.is_empty() {
-        println!("no hobby found");
+        println!("No hobbies found.");
+        return;
     }
+
+    println!("{:<50} | {:<25} | {:<20}", "Name", "Situation", "Next Plan");
+    println!("{}", "-".repeat(100));
+
     for hobby in &file_data.hobbies {
-        println!("Name:{}\t|\tSituation:{}\t|\tNext Plan:{}", hobby.name, hobby.situation, hobby.next_plan)
+        println!(
+            "{:<50} | {:<25} | {:<20}",
+            hobby.name,
+            hobby.situation,
+            hobby.next_plan
+        );
     }
 }
 
 fn list_all_routines(file_data: &mut data::DataFile) {
     if file_data.routines.is_empty() {
-        println!("no routine found");
+        println!("No routines found.");
+        return;
     }
+
+    println!("{:<50} | {:<25} | {:<20}", "Name", "Description", "Situation");
+    println!("{}", "-".repeat(100));
+
     for routine in &file_data.routines {
-        println!("Name:{}\t|\tSituation{}\t|\tDescription:{}", routine.name, routine.situation, routine.description)
+        println!(
+            "{:<50} | {:<25} | {:<20}",
+            routine.name,
+            routine.description,
+            routine.situation
+        );
     }
 }
 
