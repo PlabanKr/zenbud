436e029b3a3b54e1f7fdfd26c6a1acc0a71231ad
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-11-09 00:19:38 +0530

    Add deletion by index functionality

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -146,6 +146,13 @@ fn remove_task(commands: &[&str], file_data: &mut data::DataFile) {
     if commands.len() < 3 {
         println!("specify the task name to remove");
         return;
+    } else if commands[2] == "-idx" {
+        if commands.len() < 4 {
+            println!("no index were given")
+        } else {
+            let index: usize = commands[3].parse().unwrap();
+            file_data.tasks.remove(index);
+        }
     } else {
         let task_name = commands[2];
         file_data.tasks.retain(|ele| ele.name != task_name);
@@ -160,6 +167,13 @@ fn remove_hobby(commands: &[&str], file_data: &mut data::DataFile) {
     if commands.len() < 3 {
         println!("specify the hobby name to remove");
         return;
+    } else if commands[2] == "-idx" {
+        if commands.len() < 4 {
+            println!("no index were given")
+        } else {
+            let index: usize = commands[3].parse().unwrap();
+            file_data.hobbies.remove(index);
+        }
     } else {
         let hobby_name = commands[2];
         file_data.hobbies.retain(|ele| ele.name != hobby_name);
@@ -174,6 +188,13 @@ fn remove_routine(commands: &[&str], file_data: &mut data::DataFile) {
     if commands.len() < 3 {
         println!("specify the routine name to remove");
         return;
+    } else if commands[2] == "-idx" {
+        if commands.len() < 4 {
+            println!("no index were given")
+        } else {
+            let index: usize = commands[3].parse().unwrap();
+            file_data.routines.remove(index);
+        }
     } else {
         let routine_name = commands[2];
         file_data.routines.retain(|ele| ele.name != routine_name);
